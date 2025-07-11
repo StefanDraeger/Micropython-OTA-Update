c075ff29444877ff8c721a6158d9a9e3747f4eb9
Stefan Draeger <info@draeger-it.blog>
2025-07-11 08:18:36 +0200

    init

--- beispiel/programm.py
+++ beispiel/programm.py
@@ -1,3 +1,16 @@
+import time
+import machine
+
+ledBlau = machine.Pin(2, machine.Pin.OUT)
+ledGruen = machine.Pin(20, machine.Pin.OUT)
+
 def start():
     print("Programm gestartet")
+    while True:
+        ledBlau.value(1)
+        ledGruen.value(0)
+        time.sleep(0.5)
+        ledBlau.value(0)
+        ledGruen.value(1)
+        time.sleep(0.5)
     print("-ENDE-")
